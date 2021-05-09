06108940c6f138e1b93ed7f23b198bab1ab453cd
sanjsohal <sanj.sohal@gmail.com>
2021-05-09 19:34:19 +0530

    Observer Pattern using Java beans package

---
+++ src/com/observer/pattern/usingjavabeanspackage/MyObserver.java
@@ -0,0 +1,19 @@
+package com.observer.pattern.usingjavabeanspackage;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class MyObserver implements PropertyChangeListener {
+
+	public MyObserver(MyModel model) {
+		model.addChangeListener(this);
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
+		System.out.println("Changed property: " + event.getPropertyName() + " [old -> " + event.getOldValue()
+				+ "] | [new -> " + event.getNewValue() + "]");
+	}
+}
+
+
